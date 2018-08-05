a3ae1328202ca53cc1384ded25078918933d7458
Daniel Ojeda <211@holbertonschool.com>
2018-08-05 23:49:38 +0000

    stack

---
+++ stack.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+implements a stack using Queues.
+"""
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.len = 0
+        self.i = []
+
+    def push(self, x):
+        """
+        Push element x onto stack.
+        :type x: int
+        :rtype: void
+        """
+        self.i.append(x)
+        self.len += 1
+
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns that element.
+        :rtype: int
+        """
+        n = self.len
+        while n > 1:
+            self.i.append(self.i.pop(0))
+            n -= 1
+        self.len -= 1
+        return self.i.pop(0)
+
+    def top(self):
+        """
+        Get the top element.
+        """
+        n = self.len
+        while n > 1:
+            self.i.append(self.i.pop(0))
+            n -= 1
+        ret = self.i.pop(0)
+        self.i.append(ret)
+        return ret
+
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        """
+        return self.len == 0
