5f14b13cf70cba126cea1d85ebbc67aafcf36055
Daniel Ojeda <211@holbertonschool.com>
2018-08-06 00:17:32 +0000

    syntax fix

--- ispoweroftwo.py
+++ ispoweroftwo.py
@@ -3,7 +3,7 @@
 determines if an integer is a power of two
 """
 class Solution:
-    def isPowerOfTwo(self, n)
+    def isPowerOfTwo(self, n):
         if n < 1:
             return False
         while not n % 2:
